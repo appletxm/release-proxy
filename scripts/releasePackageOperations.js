762219d2ac99fd067e2665c1b6367479e6f865cc
tang.xiumei <tang.xiumei@ucsmy.com>
2017-11-25 09:55:55 +0800

    update version to v2.0.2

---
+++ scripts/releasePackageOperations.js
@@ -0,0 +1,140 @@
+const path = require('path')
+const fs = require('fs')
+const chalk = require('chalk')
+const filePath = path.join(__dirname, '../package.json')
+
+const packageFile = {
+  updateVersion(version) {
+    let promise
+
+    promise = new Promise((resolve) => {
+      fs.readFile(filePath, {flag: 'r+', encoding: 'utf8'}, (err, data) => {
+        if (err) {
+          throw 'There is no package.json file'
+          process.exit(1)
+        } else {
+          this.replaceVersion(version, data).then((res) => {
+            console.log(chalk.cyan('\n Replace version number success.\n'))
+
+            if (res === true) {
+              return this.addChanges()
+            }
+          }).then((res) => {
+            if (res === true) {
+              return this.commitChanges(version)
+            }
+          }).then((res) => {
+            if (res === true) {
+              return this.pushChanges()
+            }
+          }).then((res) => {
+            if (res === true) {
+              resolve(true)
+            }
+          })
+        }
+      })
+    })
+
+    return promise
+  },
+
+  replaceVersion(version, data) {
+    var promise
+
+    if (version) {
+      data = data.replace(/"version"\:\s*(.[^,]*)/, '"version": "' + version + '"')
+      promise = new Promise((resolve, reject) => {
+        fs.writeFile(filePath, data, function (err) {
+          if (err) {
+            throw 'Replace version number failed for package.json file'
+            process.exit(1)
+          }else {
+            resolve(true)
+          }
+        })
+      })
+    }
+
+    return promise
+  },
+
+  addChanges() {
+    let promise
+    let { exec } = require('child_process')
+
+    promise = new Promise((resolve) => {
+      exec('git add .', (error, stdout, stderr) => {
+        if (error) {
+          console.info('========1========')
+          console.log(`stdout: ${stdout}`)
+          console.log(`stderr: ${stderr}`)
+          console.info('========2========')
+          console.info(error)
+          throw new Error('Add package.json file failed')
+          process.exit(1)
+        }else {
+          // console.log(`stdout: ${stdout}`)
+          // console.log(`stderr: ${stderr}`)
+          console.log(chalk.cyan('\n Add package.json file success.\n'))
+
+          resolve(true)
+        }
+      })
+    })
+
+    return promise
+  },
+
+  commitChanges(version) {
+    let promise
+    let { exec } = require('child_process')
+
+    promise = new Promise((resolve) => {
+      exec('git commit -am "update version to ' + version + '"', (error, stdout, stderr) => {
+        if (error) {
+          console.info('========3========')
+          console.log(`stdout: ${stdout}`)
+          console.log(`stderr: ${stderr}`)
+          console.info('========4========')
+          console.info(error)
+          throw new Error('Commit package.json file failed')
+          process.exit(1)
+        }else {
+          // console.log(`stdout: ${stdout}`)
+          // console.log(`stderr: ${stderr}`)
+          console.log(chalk.cyan('\n Commit package.json file success.\n'))
+
+          resolve(true)
+        }
+      })
+    })
+
+    return promise
+  },
+
+  pushChanges() {
+    let promise
+    let { exec } = require('child_process')
+
+    promise = new Promise((resolve) => {
+      exec('git push origin master', (error, stdout, stderr) => {
+        console.log(`stdout: ${stderr}`)
+        if (error) {
+          throw 'Push package.json file changes to origin master failed'
+          process.exit(1)
+        } else {
+          // console.log(`stdout: ${stdout}`)
+          // console.log(`stderr: ${stderr}`)
+          console.log(chalk.cyan('\n Push package.json file to origin master branch success.\n'))
+
+          resolve(true)
+        }
+      })
+    })
+
+    return promise
+  }
+}
+
+module.exports = packageFile
---
+++ scripts/releaseTagOperations.js
@@ -0,0 +1,56 @@
+const path = require('path')
+const fs = require('fs')
+const chalk = require('chalk')
+const filePath = path.join(__dirname, '../package.json')
+
+const tagOperations = {
+  createTag(version) {
+    let promise
+    let { exec } = require('child_process')
+
+    promise = new Promise((resolve) => {
+      exec('git tag -a ' + version + ' -m "create tag version ' + version + '"', (error, stdout, stderr) => {
+        if (error) {
+          throw 'Create tag failed'
+          return
+        }
+        // console.log(`stdout: ${stdout}`)
+        // console.log(`stderr: ${stderr}`)
+        // console.log(chalk.cyan('\n Create tag version ' + version + ' success.\n'))
+
+        // this.pushNewTag(version).then((res) => {
+        //   if (res === true) {
+        //     resolve(true)
+        //   }
+        // })
+
+        resolve(true)
+      })
+    })
+
+    return promise
+  },
+
+  pushNewTag(version) {
+    let promise
+    let { exec } = require('child_process')
+
+    promise = new Promise((resolve) => {
+      exec('git push origin ' + version, (error, stdout, stderr) => {
+        if (error) {
+          throw error('Push tag to origin failed')
+          return
+        }
+        // console.log(`stdout: ${stdout}`)
+        // console.log(`stderr: ${stderr}`)
+        console.log(chalk.cyan('\n Push tag ' + version + ' to origin success.\n'))
+
+        resolve(true)
+      })
+    })
+
+    return promise
+  }
+}
+
+module.exports = tagOperations
